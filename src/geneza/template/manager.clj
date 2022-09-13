a57b9d3d356048e5a043029be96b93938d6aa7f4
kno3comma14 <enyert.vinas@gmail.com>
2022-09-13 18:40:32 -0400

    Dockerfile build function added

---
+++ src/geneza/template/manager.clj
@@ -0,0 +1,14 @@
+(ns geneza.template.manager
+  (:require [geneza.template.engine :as engine]))
+
+(def base-url "")
+
+(defn build-dockerfile
+  [template-map]
+  (let [template-file-url (str base-url "/resources/templates/kit/Dockerfile.template")
+        target-folder-path (str base-url "/resources/temp/project")
+        template-name "Dockerfile"]
+    (engine/build-template template-file-url
+                           template-map
+                           target-folder-path
+                           template-name)))
