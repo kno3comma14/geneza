aaea4da144a4b86383830c756a28eb3220b37a21
kno3comma14 <enyert.vinas@gmail.com>
2022-09-19 12:59:39 -0400

    Added a small template manager logic

--- src/geneza/template/manager.clj
+++ src/geneza/template/manager.clj
@@ -6,7 +6,7 @@
 (defn build-dockerfile
   [template-map]
   (let [template-file-url (str base-url "/resources/templates/kit/Dockerfile.template")
-        target-folder-path (str base-url "/resources/temp/project")
+        target-folder-path (str base-url "/resources/temp/" (:application-name template-map))
         template-name "Dockerfile"]
     (engine/build-template template-file-url
                            template-map
