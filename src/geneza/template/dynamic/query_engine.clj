782de78bdfdaa8d0942b3383b629addca90b3705
kno3comma14 <enyert.vinas@gmail.com>
2022-10-20 12:07:58 -0400

    Added docstrings to query-engine namespace

--- src/geneza/template/dynamic/query_engine.clj
+++ src/geneza/template/dynamic/query_engine.clj
@@ -1,17 +1,20 @@
 (ns geneza.template.dynamic.query-engine)
 
 (defn- ->quotes-string
+  "Transforms a target string to a quoted string."
   [target]
   (let [quote-wrapper "\""]
     (str quote-wrapper target quote-wrapper)))
 
 (defn- prepare-value
+  "Prepares a value depending on it's type."
   [value]
   (if (string? value)
     (->quotes-string value)
     value)) ;; Test pending
 
 (defn create-generic-find-query-section
+  "Creates a header for a query string following a Datalog structure."
   [attributes] ;; List of strings
   (let [attribute-part (reduce (fn [acc, item] (str acc " " (str "?" item)))
                                ""
@@ -19,6 +22,7 @@
     (str "'[:find" attribute-part "\n"))) ;; Test pending
 
 (defn create-generic-tuple
+  "Creates a generic tuple given tuple-name, attribute and an optional value argument."
   ([tuple-name attribute value]
    (let [entity "?e"
          complete-attribute (str ":" tuple-name "/" attribute)
@@ -35,6 +39,7 @@
      (str "[" entity " " complete-attribute " " literal-value "]")))) ;; Tests pending
 
 (defn create-generic-where-section
+  "Creates a where section given tuple-info-list map"
   [tuple-info-list]
   (let [where-literal ":where\n"]
     (reduce (fn [acc, item]
@@ -45,6 +50,7 @@
             tuple-info-list))) ;; Test pending
 
 (defn create-generic-query-string
+  "Creates a generic complete query given a tuple-info-lisp map"
   [tuple-info-list]
   (let [attribute-list (map (fn [item] (:attribute item)) tuple-info-list)
         find-query-section (create-generic-find-query-section attribute-list)
