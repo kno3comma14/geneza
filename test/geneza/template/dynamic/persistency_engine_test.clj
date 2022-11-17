6d927c3b9c30204b9933ac6fc00925b4aed12e3c
kno3comma14 <enyert.vinas@gmail.com>
2022-11-17 13:31:56 -0400

    Adding tests to fail

--- test/geneza/template/dynamic/persistency_engine_test.clj
+++ test/geneza/template/dynamic/persistency_engine_test.clj
@@ -2,5 +2,17 @@
   (:require [clojure.test :refer [deftest is testing]]
            [geneza.template.dynamic.persistency-engine :as pe]))
 
+(deftest fetch-resource-by-id-test)
+
+(deftest read-resources-test)
+
+(deftest soft-delete-resource-by-id-test)
+
+(deftest delete-resource-by-id-test)
+
+(deftest update-resource-by-id-test)
+
+(deftest create-resource-test)
+
 
 
