724e39a0fc5da24b06cd24085eba6cad20eb5807
kno3comma14 <enyert.vinas@gmail.com>
2022-11-17 11:08:25 -0400

    Few changes to manager template namespace

--- src/geneza/template/manager.clj
+++ src/geneza/template/manager.clj
@@ -1,55 +1,28 @@
 (ns geneza.template.manager
   (:require [geneza.template.engine :as engine]))
 
-(def base-url "")
 
-(defn build-dockerfile
-  [template-map]
-  (let [template-file-url (str base-url "/resources/templates/kit/Dockerfile.template")
-        target-folder-path (str base-url "/resources/temp/" (:application-name template-map))
-        template-name "Dockerfile"]
-    (engine/build-template template-file-url
-                           template-map
-                           target-folder-path
-                           template-name)))
+(defn build-project-base
+  [project-name project-description project-version project-url]
+  (let [project-structure-info {:folder-structure {"" [{:template "project.clj.template"
+                                                        :template-path "kit"
+                                                        :template-map {:project-name project-name
+                                                                       :project-description project-description
+                                                                       :project-version project-version
+                                                                       :project-url project-url}
+                                                        :file-path "resources/temp/project.clj"}
+                                                       {:template "Dockerfile.template"
+                                                        :template-path "kit"
+                                                        :template-map {:application-name project-name}
+                                                        :file-path "resources/temp/Dockerfile"}
+                                                       {:template "Makefile.template"
+                                                        :template-path "kit"
+                                                        :template-map {}
+                                                        :file-path "resources/temp/Makefile"}]}
+                                :context {}}]
+    ;; TODO fill the map with all project base templates
+    (engine/build-project-hierarchy project-structure-info)))
 
-(defn build-project-clj
-  [template-map]
-  (let [template-file-url (str base-url "/resources/templates/kit/project.clj.template")
-        target-folder-path (str base-url "/resources/temp/" (:application-name template-map))
-        template-name "Dockerfile"]
-    (engine/build-template template-file-url
-                           template-map
-                           target-folder-path
-                           template-name)))
-
-(defn build-makefile
-  [template-map]
-  (let [template-file-url (str base-url "/resources/templates/kit/Makefile.template")
-        target-folder-path (str base-url "/resources/temp/" (:application-name template-map))
-        template-name "Makefile"]
-    (engine/build-template template-file-url
-                           template-map
-                           target-folder-path
-                           template-name)))
-
-(defn babashka-edn
-  [template-map]
-  (let [template-file-url (str base-url "/resources/templates/kit/bb.edn.template")
-        target-folder-path (str base-url "/resources/temp/" (:application-name template-map))
-        template-name "bb.edn"]
-    (engine/build-template template-file-url
-                           template-map
-                           target-folder-path
-                           template-name)))
-
-
-(defn build-main
-  [template-map]
-  (let [template-file-url (str base-url "/resources/templates/kit/build.clj.template")
-        target-folder-path (str base-url "/resources/temp/" (:application-name template-map))
-        template-name "build.clj"]
-    (engine/build-template template-file-url
-                           template-map
-                           target-folder-path
-                           template-name)))
+(defn build-project-sources
+  ;; TODO: Define arguments required => Implementation using engine namespace
+  [])
