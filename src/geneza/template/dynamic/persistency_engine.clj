7a771ef21325c7f085438a9bd24acf96bd74273d
kno3comma14 <enyert.vinas@gmail.com>
2022-11-17 14:20:03 -0400

    Added function to create ns header

--- src/geneza/template/dynamic/persistency_engine.clj
+++ src/geneza/template/dynamic/persistency_engine.clj
@@ -1,8 +1,18 @@
 (ns geneza.template.dynamic.persistency-engine
-  (:require [geneza.api.schema-analyzer :as schema-analyzer]))
+  (:require [geneza.api.schema-analyzer :as schema-analyzer]
+            [geneza.template.dynamic.query-engine :as qe]))
+
+(def generic-require-block "(:require [datomic.api :as datomic-api])")
+
+(defn create-ns-header
+  [resource-name application-name]
+  (let [ns-start (str "ns " application-name ".persistency." resource-name "\n")
+        ns-final (str generic-require-block ")\n")]
+    (str ns-start ns-final)))
 
 (defn fetch-resource-by-id
-  [entity-info id])
+  [entity-info resource-name]
+  (let [query (qe/create-generic-query-string entity-info)]))
 
 (defn read-resources
   [resource-info-map])
