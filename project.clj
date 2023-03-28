52c750ca23917a9e800368656011388fc76d9389
kno3comma14 <enyert.vinas@gmail.com>
2023-03-28 18:25:20 -0400

    Adding kaocha configuration

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.cli "1.0.214"]
                  [com.datomic/datomic-free "0.9.5697"]
                  [org.clojure/java.classpath "1.0.0"]
                  [selmer "1.12.53"]
-                 [cljfmt "0.9.0"]]
+                 [cljfmt "0.9.2"]]
   :main ^:skip-aot geneza.core
   :target-path "target/%s"
   :resource-paths ["resources"]
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[lambdaisland/kaocha "1.80.1274"]]}}
+  :aliases {"kaocha" ["run" "-m" "kaocha.runner"]})
