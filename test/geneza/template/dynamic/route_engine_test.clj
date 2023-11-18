83a9081ae4cce66d0816fe95d452e330573d3208
kno3comma14 <enyert.vinas@gmail.com>
2023-11-17 20:26:14 -0500

    removing changes duplicating logic

--- test/geneza/template/dynamic/route_engine_test.clj
+++ test/geneza/template/dynamic/route_engine_test.clj
@@ -2,11 +2,4 @@
   (:require [clojure.test :refer [deftest testing is]]
             [geneza.template.dynamic.route-engine :as route-engine]))
 
-;; (deftest generate-source-routes-test
-;;   (testing "Source is generated correctly"
-;;     (let [routes-data {}
-;;           expected-result ""
-;;           actual-result (route-engine/generate-source-routes routes-data)]
-;;       (is (= expected-result actual-result)))))
 
-(def routes-map {})
